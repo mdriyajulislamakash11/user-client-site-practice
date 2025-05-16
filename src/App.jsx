bdaaf5c00c4e2ef131093cbd57cbe36cefe86104
Akash <hafejmohammadakash@gmail.com>
2025-05-16 18:50:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetch("")
+  }, [])
+
+
+  return (
+    <>
+      <h1>User Client Site Send To Data</h1>
+    </>
+  );
+}
+
+export default App;
